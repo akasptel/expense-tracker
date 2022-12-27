240c40fa2568d5f81b3e2531d0a5e555c6474198
Akash Patel <akash.patel@ets.com.au>
2022-12-27 20:04:58 +0530

    Added bottom up communication

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,57 +1,62 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from '../Expenses/ExpenseDate';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: ''
-    // });
+const ExpenseForm = (props) => {
+	// const [userInput, setUserInput] = useState({
+	//     enteredTitle: '',
+	//     enteredAmount: '',
+	//     enteredDate: ''
+	// });
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredAmount, setEnteredAmount] = useState('');
+	const [enteredDate, setEnteredDate] = useState('');
 
 	const titleChangeHandler = (event) => {
 		setEnteredTitle(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredTitle: event.target.value
-        // });
-        // setUserInput((prevState) => {
+		// setUserInput({
+		//     ...userInput,
+		//     enteredTitle: event.target.value
+		// });
+		// setUserInput((prevState) => {
 		// 			return { ...prevState, enteredTitle: event.target.value };
 		// 		});
 	};
 
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-    };
+	const amountChangeHandler = (event) => {
+		setEnteredAmount(event.target.value);
+	};
 
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-    };
+	const dateChangeHandler = (event) => {
+		setEnteredDate(event.target.value);
+	};
 
-    const submitHandler = (event) =>{
-        event.preventDefault();
+	const submitHandler = (event) => {
+		event.preventDefault();
 
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        }
-        console.log(expenseData);
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		};
+
+		props.onSaveExpenseData(expenseData);
 		setEnteredTitle('');
 		setEnteredAmount('');
 		setEnteredDate('');
-    }
+	};
 
 	return (
 		<form onSubmit={submitHandler}>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
