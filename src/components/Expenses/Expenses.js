650f3ca43f65b067be00611cab678504a049f565
Akash Patel <aakash884patel@gmail.com>
2022-12-29 22:43:54 +0530

    Reflect newly added expenses into the list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,26 +15,13 @@ const Expenses = (props) => {
 		<div>
 			<Card className="expenses">
 				<ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}/>
+				{props.expenses.map(expense => 
 				<ExpenseItem
-					title={props.expenses[0].title}
-					amount={props.expenses[0].amount}
-					date={props.expenses[0].date}
-				/>
-				<ExpenseItem
-					title={props.expenses[1].title}
-					amount={props.expenses[1].amount}
-					date={props.expenses[1].date}
-				/>
-				<ExpenseItem
-					title={props.expenses[2].title}
-					amount={props.expenses[2].amount}
-					date={props.expenses[2].date}
-				/>
-				<ExpenseItem
-					title={props.expenses[3].title}
-					amount={props.expenses[3].amount}
-					date={props.expenses[3].date}
-				/>
+					key = {expense.id}
+					title={expense.title}
+					amount={expense.amount}
+					date={expense.date}
+				/>)}
 			</Card>
 		</div>
 	);
