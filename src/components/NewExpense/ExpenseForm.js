73bc72f8b2e8240f38532b1c479889067a7cbbf6
Akash <60911828+akasptel@users.noreply.github.com>
2023-01-09 20:03:11 +0530

    Dev (#9)
    * Reflect newly added expenses into the list
    
    * Conditional rendering for expense list
    
    * Merge master into Dev (#5)
    
    * Added ExpenseForm
    
    * Added two way binding for ExpenseForm
    
    * Added bottom up communication
    
    * Added static expense filter
    
    * Dev (#4)
    
    * Reflect newly added expenses into the list
    
    * Conditional rendering for expense list
    
    Co-authored-by: Akash Patel <akash.patel@ets.com.au>
    
    * Merge Master into Dev (#7)
    
    * Added ExpenseForm
    
    * Added two way binding for ExpenseForm
    
    * Added bottom up communication
    
    * Added static expense filter
    
    * Dev (#4)
    
    * Reflect newly added expenses into the list
    
    * Conditional rendering for expense list
    
    * Dev (#6)
    
    * Reflect newly added expenses into the list
    
    * Conditional rendering for expense list
    
    * Merge master into Dev (#5)
    
    * Added ExpenseForm
    
    * Added two way binding for ExpenseForm
    
    * Added bottom up communication
    
    * Added static expense filter
    
    * Dev (#4)
    
    * Reflect newly added expenses into the list
    
    * Conditional rendering for expense list
    
    Co-authored-by: Akash Patel 
    
    Co-authored-by: Akash Patel
    
    * Added Chart
    
    * Fxied small bug

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ExpenseDate from '../Expenses/ExpenseDate';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
@@ -37,7 +36,7 @@ const ExpenseForm = (props) => {
 
 		const expenseData = {
 			title: enteredTitle,
-			amount: enteredAmount,
+			amount: +enteredAmount,
 			date: new Date(enteredDate),
 		};
 
@@ -80,6 +79,7 @@ const ExpenseForm = (props) => {
 				</div>
 			</div>
 			<div className="new-expense__actions">
+				<button type="button" onClick={props.onCancel}>Cancel</button>
 				<button type="submit">Add Expense</button>
 			</div>
 		</form>
